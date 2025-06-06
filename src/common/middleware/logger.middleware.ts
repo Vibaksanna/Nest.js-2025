738bf6017208590358afe9f931229718b91bc99b
Vibaksanna <vibak011@gmail.com>
2025-06-06 12:35:45 +0700

    update

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -3,8 +3,10 @@ import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  
   use(req: Request, res: Response, next: NextFunction) {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
     next();
   }
+  
 }
